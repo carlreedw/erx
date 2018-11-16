cb85961973ae657b53f876ebeb15e53993220dfd
Carl Reed <carl.w.reed@vumc.org>
2018-11-16 11:53:32 -0600

    data file import debugging d

--- base.php
+++ base.php
@@ -9,16 +9,15 @@ if(!defined("ENVIRONMENT")) {
 		if ($envConf[Victr_Env::ENV_CURRENT] === Victr_Env::ENV_PROD) {
 			define("ENVIRONMENT", "PROD");
 			define("PID", 77551);
-			// define("LOG_PATH", "/app001/www/redcap/plugins/erx/erx_cron_log.log");
 			define("LOG_PATH", dirname(__FILE__) . DIRECTORY_SEPARATOR . "erx_cron_log.log");
-			define("DATA_FILE_PATH", "bigdatavuhcifs.mc.vanderbilt.edu\phr\LIBRARY\Outpatient\SPECIALTY PHARMACY SERVICES\AdherenceClinic\pdc_redcap_import.csv");
+			define("DATA_FILE_PATH", "file://bigdatavuhcifs.mc.vanderbilt.edu/phr/LIBRARY/Outpatient/SPECIALTY%20PHARMACY%20SERVICES/AdherenceClinic/pdc_redcap_import.csv");
 		} elseif ($envConf[Victr_Env::ENV_CURRENT] === Victr_Env::ENV_DEV) {
 			define("ENVIRONMENT", "TEST");
 			define("PID", 1147);
-			// define("LOG_PATH", "/app001/www/redcap/plugins/erx/erx_cron_log.log");
 			define("LOG_PATH", dirname(__FILE__) . DIRECTORY_SEPARATOR . "erx_cron_log.log");
-			// define("DATA_FILE_PATH", "file://bigdatavuhcifs.mc.vanderbilt.edu/phr/LIBRARY/Outpatient/SPECIALTY%20PHARMACY%20SERVICES/AdherenceClinic/pdc_redcap_import.csv");
-			define("DATA_FILE_PATH", dirname(__FILE__) . DIRECTORY_SEPARATOR . "sample_pdc_redcap_import.csv");
+			// define("LOG_PATH", "/app001/www/redcap/plugins/erx/erx_cron_log.log");
+			// define("DATA_FILE_PATH", dirname(__FILE__) . DIRECTORY_SEPARATOR . "sample_pdc_redcap_import.csv");
+			define("DATA_FILE_PATH", "file://bigdatavuhcifs.mc.vanderbilt.edu/phr/LIBRARY/Outpatient/SPECIALTY%20PHARMACY%20SERVICES/AdherenceClinic/pdc_redcap_import.csv");
 		}
 	} else {
 		define("ENVIRONMENT", "DEV");
