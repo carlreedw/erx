8157db8b21231c0accea3471c2dd7b81fddfa3fc
Carl Reed <carl.w.reed@vumc.org>
2018-11-16 11:34:59 -0600

    changed defined log/data paths

--- base.php
+++ base.php
@@ -14,7 +14,8 @@ if(!defined("ENVIRONMENT")) {
 		} elseif ($envConf[Victr_Env::ENV_CURRENT] === Victr_Env::ENV_DEV) {
 			define("ENVIRONMENT", "TEST");
 			define("PID", 1147);
-			define("LOG_PATH", "/app001/www/redcap/plugins/erx/erx_cron_log.log");
+			// define("LOG_PATH", "/app001/www/redcap/plugins/erx/erx_cron_log.log");
+			define("LOG_PATH", dirname(__FILE__) . DIRECTORY_SEPARATOR . "erx_cron_log.log");
 			define("DATA_FILE_PATH", "\\bigdatavuhcifs.mc.vanderbilt.edu\phr\LIBRARY\Outpatient\SPECIALTY PHARMACY SERVICES\AdherenceClinic\pdc_redcap_import.csv");
 			// define("DATA_FILE_PATH", dirname(__FILE__) . DIRECTORY_SEPARATOR . "sample_pdc_redcap_import.csv");
 		}
