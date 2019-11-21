40103bd0f218023b7d2e7c0d2b020993222b739b
Carl Reed <carlreedw@gmail.com>
2019-11-21 15:53:11 -0600

    updated plugin 11/21, added once.php, copyImportDates is called after import to copy [import_date] to [importdatecopy1]

--- base.php
+++ base.php
@@ -23,6 +23,6 @@ if(!defined("ENVIRONMENT")) {
 		define("RC_CONNECT_PATH", "C:\\xampp\\htdocs\\redcap\\redcap_connect.php");
 		define("AUTOLOAD_PATH", "C:\\xampp\\htdocs\\redcap\\plugins\\erx\\vendor\\autoload.php");
 		// define("PID", 53);
-		define("PID", 54);
+		define("PID", 33);
 	}
 }
